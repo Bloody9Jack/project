6738f0dd21a4885122ac1f0d0532ea6942de9a38
Kirill <kravtsov_728@mail.ru>
2022-08-02 18:00:20 +0300

    second part of project

--- project/script.js
+++ project/script.js
@@ -11,12 +11,31 @@ const personalMovieDB = {
     privat: false
 };
 
-const a = prompt('один из последних просмотренный фильмов?', ''),
-    b = prompt("на сколько оцените его?", ''),
-    c = prompt("один из последних просмотренный фильмов?", ''),
-    d = prompt("на сколько оцените его?", '');
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+for (let i = 0; i < 1; i++) {
+    const a = prompt('один из последних просмотренный фильмов?', ''),
+        b = prompt("на сколько оцените его?", '');
+
+    if (a != null && b != null && a != '' && b != "" && a.length < 50) {
+        personalMovieDB.movies[a] = b;
+        console.log('done');
+    } else {
+        console.log('error');
+        i--;
+    }
+
+    personalMovieDB.movies[a] = b;
+
+}
+
+if (personalMovieDB.count < 10) {
+    console.log("просмотренно мало фильмов");
+} else if (personalMovieDB.count > 10 && personalMovieDB < 30) {
+    console.log('вы классический зритель');
+} else if (personalMovieDB.count >= 30) {
+    console.log('вы киноман');
+} else {
+    console.log('error');
+}
 
 console.log(personalMovieDB);
